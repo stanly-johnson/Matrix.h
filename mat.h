2dbef506ad8378272fadd7f462096d45a7e23a2d
stanly-johnson <stanlyjohnson01@gmail.com>
2015-08-28 21:18:11 +0530

    mul

--- mat.h
+++ mat.h
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-int matadd(int *mat_one, int mat_one_size, int *mat_two, int mat_two_size)
+int* matadd(int *mat_one, int mat_one_size, int *mat_two, int mat_two_size)
 {
 	int mat_sum[3][3];
 	for(int index_one=0;index_one<20;index_one++)
@@ -16,8 +16,16 @@ int matadd(int *mat_one, int mat_one_size, int *mat_two, int mat_two_size)
 }
 
 
-int matmul(int *mat_one, int mat_one_row, int mat_one_col, int *mat_two, int mat_two_row, int mat_two_col)
+int* matmul(int *mat_one, int mat_one_row, int mat_one_col, int *mat_two, int mat_two_row, int mat_two_col)
 {
 	int mat_mul[3][3];
-	for
+	for(int index_one=0;index_one<20;index_one++)
+	{
+		for(int index_two=0;index_two<20;index_two++)
+		{
+			mat_mul[index_one][index_two]=mat_mul[index_one][index_two]+(mat_one[index_one][index_two]*mat_two[index_two][index_one])
+		}
+  }
+
+	return *mat_mul
 }
