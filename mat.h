151d9251a964091c7b9800638d08d5a093638976
stanly-johnson <stanlyjohnson01@gmail.com>
2015-08-28 21:26:57 +0530

    trans

--- mat.h
+++ mat.h
@@ -23,9 +23,24 @@ int* matmul(int *mat_one, int mat_one_row, int mat_one_col, int *mat_two, int ma
 	{
 		for(int index_two=0;index_two<20;index_two++)
 		{
-			mat_mul[index_one][index_two]=mat_mul[index_one][index_two]+(mat_one[index_one][index_two]*mat_two[index_two][index_one])
+			mat_mul[index_one][index_two]=mat_mul[index_one][index_two]+(mat_one[index_one][index_two]*mat_two[index_two][index_one]);
 		}
   }
 
-	return *mat_mul
+	return *mat_mul;
+}
+
+
+int* mat_trans(int *mat_input, int size)
+{
+	int mat_trans_result;
+	for(int index_one=0;index_one<20;index_one++)
+	{
+		for(int index_two=0;index_two<20;index_two++)
+		{
+			mat_trans_result[index_two][index_one]=mat_input[index_one][index_two];
+		}
+	}
+
+	return *mat_trans_result;
 }
