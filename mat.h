6b3f4103117bea4b2a00b9a59cae560d677c00b7
stanly-johnson <stanlyjohnson01@gmail.com>
2015-09-27 19:35:39 +0530

    mat-2-add

--- mat.h
+++ mat.h
@@ -51,4 +51,26 @@ int* mat_trans(int *mat_input, int size)
 int* 2matadd(int *mat_one, int mat_one_size, int* mat_two, int mat_two_size)
 {
 	int mat_sum[2][2];
-	for
+	for(int index_one=0;index_one<20;index_one++)
+	{
+		for(int index_two=0;index_two<20;index_two++)
+		{
+			mat_sum[index_one][index_two]=mat_one[index_one][index_two]+mat_two[index_one][index_two];
+
+		}
+	}
+	return *mat_sum;
+}
+
+int* 2matmul(int *mat_one, int mat_one_row, int mat_one_col, int *mat_two, int mat_two_row, int mat_two_col)
+{
+	int mat_mul[2][2];
+	for(int index_one=0;index_one<20;index_one++)
+	{
+		for(int index_two=0;index_two<20;index_two++)
+		{
+			mat_mul[index_one][index_two]=mat_mul[index_one][index_two]+(mat_one[index_one][index_two]*mat_two[index_two][index_one]);
+		}
+  }
+	return *mat_mul;
+}
